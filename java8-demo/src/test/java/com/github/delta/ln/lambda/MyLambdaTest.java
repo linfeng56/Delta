3e4f2f2f1c7c5100ed6928c04d8c391152bc56ab
yzln <2696726246@qq.com>
2022-09-15 19:52:34 +0800

    java8测试

--- java8-demo/src/test/java/com/github/delta/ln/lambda/MyLambdaTest.java
+++ java8-demo/src/test/java/com/github/delta/ln/lambda/MyLambdaTest.java
@@ -2,6 +2,8 @@ package com.github.delta.ln.lambda;
 
 import org.junit.Test;
 
+import java.util.Comparator;
+
 public class MyLambdaTest {
 
     @Test
@@ -11,8 +13,14 @@ public class MyLambdaTest {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         Runnable r = () -> System.out.println("哈哈");
         r.run();
+    }
+
+    @Test
+    public void test3() {
+        Comparator<Integer> com = (x, y) -> Integer.compare(x, y);
+        com.compare(1,3);
     }
 }
